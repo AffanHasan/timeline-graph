bde5f3f9a3c54e749597dc1ce52a5fedcfde9529
Affan Hassan <ahassan@NIS-PK549s-MacBook-Pro.local>
2023-05-23 17:14:43 +0500

    Added test cases for Prophet Muhammad S.A.W

--- src/test/java/timeline/graph/ProphetMuhammadSAWTest.java
+++ src/test/java/timeline/graph/ProphetMuhammadSAWTest.java
@@ -4,6 +4,7 @@
  */
 package timeline.graph;
 
+import java.util.Map;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -22,54 +23,65 @@ public class ProphetMuhammadSAWTest {
         final TimelineGraph timelineGraph = new StandardTimelineGraph();
         this.g = timelineGraph.getGraphTraversalSource();
     }
-
+    
     @Test
-    public void shouldAssertThatProphetIsBornIn53BH() {
+    public void prophetMuhammadSAWBirthEventShouldExist() {
         // There should be only one event named Birth Of Prophet Muhammad S.A.W
         assertEquals(1L, this.getBirthEventVertex().count().next());
-       
-        // Verify that there are only 2 references to birth currently
-        assertEquals(2L, this.getBirthEventVertex() //
-            .outE().hasLabel("happened_in").count().next());
+    }
+
+    @Test
+    public void WikipediaReferenceForBirthIn53BHShouldExist() {
         
-        // Verify that one of the references is for wikipedia
-        assertEquals(1, this.getBirthEventVertex() //
-            .outE().hasLabel("happened_in").toList().stream().filter(e->
-            e.value("source").equals("wikipedia") &&
-            e.value("reference").equals("https://en.wikipedia.org/wiki/Muhammad")).count());
-        
-        // Verify that one of the references is for Al azami book "History Of Quranic Text"
-        assertEquals(1, this.getBirthEventVertex() //
-            .outE().hasLabel("happened_in").toList().stream().filter(e->
-            e.value("source").equals("The History Of Quranic Text From Revelation To Compilation By Mustafa Al Aazami") &&
-            e.value("reference").equals("Jan 2003 Page 23")).count());
+        this.getBirthEventVertex().as("birthEvent") //
+            .out("borned_in").count().next();
+//            .has("source", "wikipedia").as("wikipediaReference") //
+//            .outV().as("hijriYear") //
+//            .select("birthEvent", "wikipediaReference", "hijriYear") //
+//            .select("birthEvent", "wikipediaReference") //
+//            .next();
+        System.out.println(this.getBirthEventVertex()
+            .out("borned_in").count().next());
+
+//        
+//        // Verify that one of the references is for wikipedia
+//        assertEquals(1, this.getBirthEventVertex() //
+//            .outE().hasLabel("borned_in").toList().stream().filter(e->
+//            e.value("source").equals("wikipedia") &&
+//            e.value("reference").equals("https://en.wikipedia.org/wiki/Muhammad")).count());
+//        
+//        // Verify that one of the references is for Al azami book "History Of Quranic Text"
+//        assertEquals(1, this.getBirthEventVertex() //
+//            .outE().hasLabel("borned_in").toList().stream().filter(e->
+//            e.value("source").equals("The History Of Quranic Text From Revelation To Compilation By Mustafa Al Aazami") &&
+//            e.value("reference").equals("Jan 2003 Page 23")).count());
     }
     
     @Test
-    public void shouldAssertThatProphetIsdiedIn11AH() {
+    public void shouldAssertThatProphetDiedIn11AHWikipedia() {
         // There should be only one event named Birth Of Prophet Muhammad S.A.W
         assertEquals(1L, getDeathEventVertex().count().next());
        
         // Verify that there are only 2 references to birth currently
         assertEquals(2L, getDeathEventVertex() //
-            .outE().hasLabel("happened_in").count().next());
+            .outE().hasLabel("died_in").count().next());
         
         // Verify that one of the references is for wikipedia
         assertEquals(1, 
                 getDeathEventVertex() //
-            .outE().hasLabel("happened_in").toList().stream().filter(e->
+            .outE().hasLabel("died_in").toList().stream().filter(e->
             e.value("source").equals("wikipedia") &&
             e.value("reference").equals("https://en.wikipedia.org/wiki/Muhammad")).count());
         
         // Verify that one of the references is for Al azami book "History Of Quranic Text"
         assertEquals(1, getDeathEventVertex() //
-            .outE().hasLabel("happened_in").toList().stream().filter(e->
+            .outE().hasLabel("died_in").toList().stream().filter(e->
             e.value("source").equals("The History Of Quranic Text From Revelation To Compilation By Mustafa Al Aazami") &&
             e.value("reference").equals("Jan 2003 Page 23")).count());
     }
     
     private GraphTraversal<Vertex, Vertex> getBirthEventVertex() {
-        return this.g.V().has("event", "name", "Death Of Prophet Muhammad S.A.W");
+        return this.g.V().has("event", "name", "Birth Of Prophet Muhammad S.A.W");
     }
     
     private GraphTraversal<Vertex, Vertex> getDeathEventVertex() {
